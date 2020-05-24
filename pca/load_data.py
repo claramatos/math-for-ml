5629e80f07a92ce0d80e452cfbd619f1a2318df8
Clara Matos <anaclaramatos@hotmail.com>
2020-05-24 22:49:26 +0100

    add solutions for pca notebooks

---
+++ pca/load_data.py
@@ -0,0 +1,11 @@
+def load_mnist():
+    mnist_path = "mnist-original.mat"
+    from scipy.io import loadmat
+    mnist_raw = loadmat(mnist_path)
+    mnist = {
+        "data": mnist_raw["data"].T,
+        "target": mnist_raw["label"][0],
+        "COL_NAMES": ["label", "data"],
+        "DESCR": "mldata.org dataset: mnist-original",
+    }
+    return mnist
